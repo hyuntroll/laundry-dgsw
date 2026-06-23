d7f163a421071914d1374c49807cc1f7d9cbf497
hyuntroll <hsm200905292@gmail.com>
2026-06-23 15:00:17 +0900

    exception handler

---
+++ src/main/java/com/dgsw/laundry/dto/ResponseDto.java
@@ -0,0 +1,12 @@
+package com.dgsw.laundry.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Setter
+@Getter
+public class ResponseDto {
+    private String message;
+}
---
+++ src/main/java/com/dgsw/laundry/exception/QuickExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.dgsw.laundry.exception;
+
+import com.dgsw.laundry.dto.ResponseDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class QuickExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponseDto> handleValidation(MethodArgumentNotValidException ex) {
+        String errorMessage = ex.getBindingResult().getFieldErrors().stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+        return ResponseEntity.badRequest().body(new ResponseDto("입력 오류 - " + errorMessage));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ResponseDto> handleGeneric(Exception ex) {
+        return ResponseEntity.status(500).body(new ResponseDto("서버 오류"));
+    }
+}
