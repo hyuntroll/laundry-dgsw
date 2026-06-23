ff3edc0aecbe7040c44a01768cd16a690032691c
jdw09 <razion0304@naver.com>
2026-06-23 13:45:39 +0900

    feat: find machines by floor

--- src/main/java/com/dgsw/laundry/repository/MachineRepository.java
+++ src/main/java/com/dgsw/laundry/repository/MachineRepository.java
@@ -4,6 +4,9 @@ import com.dgsw.laundry.entity.Machine;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MachineRepository extends JpaRepository<Machine, Long> {
+    List<Machine> findMachineByFloor(Integer floor);
 }
--- src/main/java/com/dgsw/laundry/service/MachineService.java
+++ src/main/java/com/dgsw/laundry/service/MachineService.java
@@ -7,6 +7,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @Slf4j
 public class MachineService {
@@ -21,5 +23,9 @@ public class MachineService {
         machine.setReserved(false);
         machineRepository.save(machine);
         return machine;
+    }
+
+    public List<Machine> getMachinesByFloor(Integer floor) {
+        return machineRepository.findMachineByFloor(floor);
     }
 }
